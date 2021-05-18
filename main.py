34af5d349f6821cd36550bab780939bddb3fd800
Baptiste D <drouillet.baptiste@gmail.com>
2021-05-18 10:07:23 +0200

    init

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
+from flask import Flask, render_template, request, redirect, session, url_for
+from flask_session import Session
+import os
+
+# Création de l'application
+app = Flask(__name__)
+
+# matrice du jeu
 jeu = [["", "", "", "", "", "", ""], ["", "", "", "", "", "", ""], ["", "", "", "", "", "", ""], [
     "", "", "", "", "", "", ""], ["", "", "", "", "", "", ""], ["", "", "", "", "", "", ""]]
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', matrice=jeu)
+
+
+if __name__ == "__main__":
+    app.secret_key = os.urandom(24)
+    # Lancement de l'application, à l'adresse 127.0.0.0 et sur le port 3000
+    app.run(host="127.0.0.1", port=3000, debug=True)
