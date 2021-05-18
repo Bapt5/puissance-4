ccc4a265e60193dccecd6419bf8c01aceb80c07f
Mflavien01 <72614724+Mflavien01@users.noreply.github.com>
2021-05-18 13:26:59 +0200

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,8 @@
-alert(document.getElementById('fleche').value);
-
 function touche(e){
   var touche = event.keyCode;
       if (touche==37){
-           document.getElementById('fleche').style.marginLeft ="6%" ;
+           document.getElementById('fleche').style.marginLeft -=6 ;
       }if (touche==39){
-           document.getElementById('fleche').style.marginLeft ="6%" ;
+           document.getElementById('fleche').style.marginLeft +=6 ;
       }
     }
